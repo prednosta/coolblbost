c2fed71b46ccfbc25de881ea90a4fbfe4595133e
Břetislav Král <bretakral@gmail.com>
2019-07-20 19:04:54 +0200

    update
    a little security hack

--- coolmoney.sql
+++ coolmoney.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Počítač: 127.0.0.1:3306
--- Vytvořeno: Čtv 18. čec 2019, 18:59
+-- Vytvořeno: Čtv 27. čen 2019, 16:22
 -- Verze serveru: 5.7.26
 -- Verze PHP: 7.2.18
 
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `uzivatel` (
   `email` text CHARACTER SET utf8 NOT NULL,
   `heslo` text COLLATE utf8_czech_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 --
 -- Vypisuji data pro tabulku `uzivatel`
@@ -63,66 +63,7 @@ CREATE TABLE IF NOT EXISTS `uzivatel` (
 
 INSERT INTO `uzivatel` (`id`, `jmeno`, `prijmeni`, `telefon`, `email`, `heslo`) VALUES
 (26, 'Břetislav', 'Král', '732432543', 'bre@ta.cz', 'asdf'),
-(27, 'Tamten', 'Hajnej', '843234543', 'tam@haj.ne', 'asdfg'),
-(28, 'Tajny', 'Hajny', '843232919', 'tajny@hajny.ca', 'asdf'),
-(29, 'Beny', 'Saryk', '843943', 'benysaryk@gmail.com', 'asdf');
-
--- --------------------------------------------------------
-
---
--- Struktura tabulky `zakazky`
---
-
-DROP TABLE IF EXISTS `zakazky`;
-CREATE TABLE IF NOT EXISTS `zakazky` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `datum_vytvoreni` date NOT NULL COMMENT 'datum vytvoreni zakazky',
-  `max_cena` int(11) DEFAULT NULL COMMENT 'maximalni cena',
-  `popis` text NOT NULL,
-  UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
-
---
--- Vypisuji data pro tabulku `zakazky`
---
-
-INSERT INTO `zakazky` (`id`, `datum_vytvoreni`, `max_cena`, `popis`) VALUES
-(1, '2019-07-18', 5000, 'Chyba levého předního světla - bliká'),
-(2, '2019-07-18', 5000, 'Chyba levého předního světla - nebliká'),
-(3, '2019-07-18', 25000, 'Chyba startování - nejede to'),
-(4, '2019-07-20', 56030, 'Chyba stěračů - nejede to'),
-(5, '2019-07-20', 56030, 'Chyba stěračů - stírají moc'),
-(6, '2019-07-20', 56030, 'Klepe motor - moc');
-
--- --------------------------------------------------------
-
---
--- Struktura tabulky `zakaznik`
---
-
-DROP TABLE IF EXISTS `zakaznik`;
-CREATE TABLE IF NOT EXISTS `zakaznik` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nazev_firmy` text COLLATE utf8_czech_ci NOT NULL,
-  `ico` int(9) NOT NULL,
-  `telefon` int(15) NOT NULL,
-  `adresa` text COLLATE utf8_czech_ci NOT NULL,
-  `jednatel` text COLLATE utf8_czech_ci NOT NULL,
-  `datum_registrace` date NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci COMMENT='seznam zákazníků';
-
---
--- Vypisuji data pro tabulku `zakaznik`
---
-
-INSERT INTO `zakaznik` (`id`, `nazev_firmy`, `ico`, `telefon`, `adresa`, `jednatel`, `datum_registrace`) VALUES
-(6, 'břeťa soft s.r.o.', 12345678, 722797874, 'L hosaka 19, Ostrava - Jih', 'Břetislav Král', '2019-07-04'),
-(5, 'břeťa soft s.r.o.', 12345678, 722797874, 'L hosaka 19, Ostrava - Jih', 'Břetislav Král', '2019-07-04'),
-(4, 'freak soft s.r.o.', 32158465, 722797874, 'L derata 19, Ostrava - Jih', 'Plav Král', '2019-07-04'),
-(7, 'břeťa soft s.r.o.', 12345678, 722797874, 'L hosaka 19, Ostrava - Jih', 'Břetislav Král', '2019-07-04'),
-(8, 'břeťa soft s.r.o.', 12345678, 722797874, 'L hosaka 19, Ostrava - Jih', 'Břetislav Král', '2019-07-04'),
-(9, 'freak soft s.r.o.', 32158466, 722797874, 'L derata 19, Ostrava - Jih', 'Plav Král', '2019-07-04');
+(27, 'Tamten', 'Hajnej', '843234543', 'tam@haj.ne', 'asdfg');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
