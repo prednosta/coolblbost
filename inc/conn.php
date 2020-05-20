1f36ad04a6817e6acae7a4d42453817f661cc961
Břetislav Král <bretakral@gmail.com>
2020-05-20 11:18:31 +0200

    Add files via upload

---
+++ inc/conn.php
@@ -0,0 +1,27 @@
+<?php
+// pripojeni k databazi
+
+
+$servername = "localhost"; // jmeno serveru
+$username = "breta";        // uzivatelske jmeno
+$password = "xT4y7Zps";            // heslo
+
+$dbname = "coolmoney";     // nazev databaze
+
+// pripojeni
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// kontrola pripojeni
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//změna charset
+if (!$conn->set_charset("utf8")) {
+	printf("utf8 nenacten: %s<br />", $conn->error);
+	exit();
+} else {
+	$conn->character_set_name();
+}
+
+?> 
